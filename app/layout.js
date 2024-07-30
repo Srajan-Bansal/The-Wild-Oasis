98b7fadb7de49c4031d6751bbcab629dfabcd139
Srajan Bansal <srajanbansal999@gmail.com>
2024-07-31 02:33:53 +0530

    Building The Wild Oasis

--- app/layout.js
+++ app/layout.js
@@ -1,20 +1,34 @@
-import Logo from '@/app/_components/Logo';
-import Navigation from '@/app/_components/Navigation';
+import Header from '@/app/_components/Header';
+
+import { Josefin_Sans } from 'next/font/google';
+
+const josefin = Josefin_Sans({
+	subsets: ['latin'],
+	display: 'swap',
+});
+
+import '@/app/_styles/globals.css';
 
 export const metadata = {
-	title: 'The Wild Oasis',
+	// title: 'The Wild Oasis',
+	title: {
+		template: '%s / The Wild Oasis',
+		default: 'Welcome / The Wild Oasis',
+	},
+	description:
+		'Luxurios cabin hotel, located in the Goa, surrounded by beautiful occean',
 };
 
 export default function RootLayout({ children }) {
 	return (
 		<html lang='en'>
-			<body>
-				<header>
-					<Logo />
-					<Navigation />
-				</header>
-				<main>{children}</main>
-				<footer>Copyright by The Wild Oasis</footer>
+			<body
+				className={`${josefin.className} bg-primary-950 text-primary-100 min-h-screen flex flex-col`}
+			>
+				<Header />
+				<div className='flex-1 px-8 py-12 grid'>
+					<main className='max-w-7xl mx-auto w-full'>{children}</main>
+				</div>
 			</body>
 		</html>
 	);
